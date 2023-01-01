8f06603129653c571ae9c689bfc87b15555d5640
asiyahzaheem <asiyahzaheem2006@gmail.com>
2023-01-01 20:15:36 +0530

    Webhook checkout trial 1

--- controllers/purchaseController.js
+++ controllers/purchaseController.js
@@ -83,5 +83,5 @@ exports.webhookCheckout = (req, res, next) => {
   console.log(event.data.object);
   if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
-  res.status(200).json({ received: 'true ' });
+  res.status(200).json({ received: true });
 };
